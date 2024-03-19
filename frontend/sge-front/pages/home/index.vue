bdbff3fd4fb4c4e6f48e20cfb76e9396996e8d47
Rhuan Ribeiro <rhuan.augusto.ribeiro@gmail.com>
2024-03-19 20:22:05 -0300

    SGE - FullStack

---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+const { signOut } = useAuth();
+
+definePageMeta({
+    middleware: 'auth'
+})
+
+const submitLogout = () => {
+
+    signOut({redirect: false}).then(()=>{
+
+        console.log("Successfully logout!")
+        navigateTo('/');
+    })
+        .catch((error)=>{
+        console.log("Error when trying to logout: ", error);
+    }); 
+
+}
+</script>
+
+
+<template>
+
+<div>
+    <h1>Minha tela Home</h1>
+    
+    <button type="submit" @click="submitLogout" class="customButton">Sair</button>
+</div>
+
+</template>
+
+<style >
+
+
+</style>
